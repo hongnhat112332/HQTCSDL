ac0605adc1dba643492fd514170c5d28b5051336
hongnhat112332 <128466195+hongnhat112332@users.noreply.github.com>
2023-04-18 17:09:29 +0700

    Add files via upload

---
+++ lab10.sql
@@ -0,0 +1,21 @@
+﻿use QuanLyBanHang
+go
+
+--Thêm dữ liệu cho bảng NhanVien sau đó thực hiện full backup
+insert into NhanVien
+values ('NV01','Tran Hong Nhat','Nam','dong nai','0328634871','nhat@gmail.com','ke toan');
+
+BACKUP DATABASE QuanLyBanHang TO DISK = 'D:\ TranHongNhat' WITH INIT;
+
+
+--Thêm dữ liệu cho bảng NhanVien sau đó thực hiện different backup
+
+BACKUP DATABASE QuanLyBanHang TO DISK = 'D:\ TranHongNhat' WITH differential, format;
+
+--thực hiện log backup
+backup log QuanLyBanHang to disk = 'D:\ TranHongNhat'WITH INIT;
+
+--khôi phục dữ liệu với file backup
+RESTORE LOG QuanLyBanHang
+FROM DISK = 'D:\ TranHongNhat\QlBanHang'
+WITH RECOVERY
